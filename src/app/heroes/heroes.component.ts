9d02200f79216a4e6421c268dea39bc28849f58a
Song <songgalmegi@naver.com>
2018-03-14 15:52:15 +0900

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,23 @@ export class HeroesComponent implements OnInit {
     name:'Windstrom'
   }
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero:Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero:Hero):void{
     //console.log(hero);
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
